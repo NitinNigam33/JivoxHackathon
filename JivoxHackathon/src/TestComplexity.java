e2fefd3d4ecd9251dc9621667a455122dba87c2f
Nitin <nitin.nigam@sellerworx.com>
2016-08-06 15:36:30 +0530

    test case modification for heapSort and quickSort

--- JivoxHackathon/src/TestComplexity.java
+++ JivoxHackathon/src/TestComplexity.java
@@ -18,11 +18,21 @@ public class TestComplexity extends Heap {
 		}
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date = new Date();
-		System.out.println( "before sorting " + dateFormat.format(date));
+		System.out.println( "before sorting from Heap " + dateFormat.format(date));
 		Heap heapSort = new Heap();
 		heapSort.sort(arr);
 	
+		DateFormat dateFormat1 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date1 = new Date();
-		System.out.println( " after sorting " +dateFormat.format(date1));
+		System.out.println( "after sorting from Heap " + dateFormat1.format(date1));
+		
+		DateFormat dateFormat11 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date11 = new Date();
+		System.out.println( "before sorting from quick sort " + dateFormat11.format(date11));
+		Quick_Sort qsort = new Quick_Sort();
+		DateFormat dateFormat111 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date111 = new Date();
+		System.out.println( "after sorting from quick sort " + dateFormat111.format(date111));
+		
 	}
 }

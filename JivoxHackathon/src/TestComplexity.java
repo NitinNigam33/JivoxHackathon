78917e86c30d5d5613b5b939cc27a88fd2fdb15c
Nitin <nitin.nigam@sellerworx.com>
2016-08-06 15:40:31 +0530

    Change to BigInteger

--- JivoxHackathon/src/TestComplexity.java
+++ JivoxHackathon/src/TestComplexity.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,7 +9,8 @@ public class TestComplexity extends Heap {
 	public static void main(String[] args) {
 		System.out.println("enter the range 0 to n for test the sorting algo");
 		Scanner sc = new Scanner(System.in);
-		int range = sc.nextInt();
+		BigInteger len = sc.nextBigInteger();
+		int range = len.intValue();
 		Integer[] arr =  new Integer[range];
 		
 		Random rn = new Random();
@@ -18,21 +20,21 @@ public class TestComplexity extends Heap {
 		}
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date = new Date();
-		System.out.println( "before sorting from Heap " + dateFormat.format(date));
+		System.out.println( "Time before sorting from HeapSort " + dateFormat.format(date));
 		Heap heapSort = new Heap();
 		heapSort.sort(arr);
 	
 		DateFormat dateFormat1 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date1 = new Date();
-		System.out.println( "after sorting from Heap " + dateFormat1.format(date1));
+		System.out.println( "Time after sorting from HeapSort " + dateFormat1.format(date1));
 		
 		DateFormat dateFormat11 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date11 = new Date();
-		System.out.println( "before sorting from quick sort " + dateFormat11.format(date11));
+		System.out.println( " Time before sorting from quickSort " + dateFormat11.format(date11));
 		Quick_Sort qsort = new Quick_Sort();
 		DateFormat dateFormat111 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date111 = new Date();
-		System.out.println( "after sorting from quick sort " + dateFormat111.format(date111));
+		System.out.println( " Time after sorting from quickSort " + dateFormat111.format(date111));
 		
 	}
 }
